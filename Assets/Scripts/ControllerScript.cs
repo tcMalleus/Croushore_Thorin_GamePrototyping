bc89b36a21818c0f766ce547805ee09d64d0377c
tcMalleus <thorinuc@gmail.com>
2018-05-01 16:36:41 -0400

    Ugly but mostly done

---
+++ Assets/Scripts/ControllerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour {
+
+    public float MoveSpeed;
+
+    private Rigidbody2D _rigid;
+
+	// Use this for initialization
+	void Start () {
+
+        _rigid = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("space"))
+        {
+            _rigid.AddForce(Vector2.left * MoveSpeed, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerBounceScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBounceScript : MonoBehaviour {
+
+    public float BounceForce;
+
+    private Rigidbody2D _rigid;
+
+
+    // Use this for initialization
+    void Start () {
+
+        _rigid = GetComponent<Rigidbody2D>();
+
+        _rigid.AddForce(Vector2.up * BounceForce, ForceMode2D.Impulse);
+
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "BounceTop")
+        {
+            _rigid.velocity = Vector2.zero;
+            _rigid.AddForce(Vector2.down * BounceForce, ForceMode2D.Impulse);
+        }
+
+        if (collision.gameObject.tag == "BounceBottom")
+        {
+            _rigid.velocity = Vector2.zero;
+            _rigid.AddForce(Vector2.up * BounceForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Kill")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
